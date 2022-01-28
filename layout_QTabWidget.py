00d09504c9a6ada4ffd2798d0e6ce1794ecf356e
user.email <garciamarinb@gmail.com>
2022-01-28 14:57:37 +0100

    Updating files
    Updating layout_QTabWidget.py

--- layout_QTabWidget.py
+++ layout_QTabWidget.py
@@ -34,6 +34,9 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My Awesome App")
        
         tabs = QTabWidget()
+        tabs.setDocumentMode(True)
+        tabs.setTabPosition(QTabWidget.East)
+        tabs.setMovable(True)
         
         for n, color in enumerate(['red', 'green', 'blue', 'yellow']):
             tabs.addTab( Color( color ), color )
